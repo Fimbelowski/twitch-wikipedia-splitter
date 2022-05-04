7d1ed87baaa21a127b330fb7e4610f70dcad99f5
Christopher <ChrFimbel@gmail.com>
2022-05-04 00:27:06 -0600

    Simplify structure.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,63 @@
 <template>
-  <TextSplitter />
+  <div
+    class="input"
+  >
+    <BaseTextarea
+      id="input"
+      v-model="state.input"
+      label="Input"
+    />
+    <BaseCheckbox
+      id="remove-citations"
+      v-model="state.removeCitations"
+      label="Remove Citations"
+    />
+    <BaseCheckbox
+      id="remove-parentheticals"
+      v-model="state.removeParentheticals"
+      label="Remove Parentheticals"
+    />
+  </div>
+  <div>
+    <BaseTextarea
+      id="output"
+      label="Output"
+      :model-value="parsedInput"
+      readonly
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import TextSplitter from '@/components/TextSplitter.vue';
+import { computed, reactive } from 'vue';
+import BaseCheckbox from '@/components/BaseCheckbox.vue';
+import BaseTextarea from '@/components/BaseTextarea.vue';
+
+const state = reactive({
+  input: '',
+  removeCitations: true,
+  removeParentheticals: true,
+});
+
+const parsedInput = computed(() => {
+  let parsed = state.input;
+
+  if (state.removeCitations) {
+    parsed = parsed.replace(/\[[^\]*]\]/gm, '');
+  }
+
+  if (state.removeParentheticals) {
+    parsed = parsed.replace(/\([^)]*\)/gm, '');
+  }
+
+  // Fix orphaned commas
+  parsed = parsed.replace(/ ,/gm, ',');
+
+  // Trim consecutive whitespace
+  parsed = parsed.replace(/ {2}/gm, ' ');
+
+  return parsed.trim();
+});
 </script>
 
 <style>
@@ -17,7 +71,12 @@ import TextSplitter from '@/components/TextSplitter.vue';
   display: flex;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   justify-content: center;
+  padding-top: 25px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.input {
+  margin-right: 25px;
 }
 </style>
