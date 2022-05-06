56bab22f70dc118eb1ae888ea85f96c19c4a69fd
Christopher <ChrFimbel@gmail.com>
2022-05-05 21:48:30 -0600

    Added CheckboxInput.

---
+++ src/components/CheckboxInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <BaseInput
+    :id="id"
+    :label="label"
+    :model-value="modelValue"
+    type="checkbox"
+    @update:model-value="onUpdateModelValue"
+  />
+</template>
+
+<script setup lang="ts">
+import BaseInput from '@/components/BaseInput.vue';
+
+defineProps<{
+  id: string,
+  label: string,
+  modelValue: boolean,
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void,
+}>();
+
+function onUpdateModelValue(value: boolean) {
+  emit('update:modelValue', value);
+}
+</script>
