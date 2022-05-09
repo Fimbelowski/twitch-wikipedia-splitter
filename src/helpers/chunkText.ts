1731dfa01b1819eec21c5ef1f459e25eaa600f78
Christopher <ChrFimbel@gmail.com>
2022-05-09 01:01:55 -0600

    Improved chunking behaviors.

--- src/helpers/chunkText.ts
+++ src/helpers/chunkText.ts
@@ -1,53 +1,65 @@
 import { ChunkingBehaviors } from '../types/ChunkingBehaviors';
+const hardSentenceBoundaryRegexp = /[.?!] /gm;
+const softSentenceBoundaryRegexp = /[,;-] /gm;
 
-export default function chunkText(input: string, chunkingBehavior: ChunkingBehaviors, balkingDistance: number) {
-  if (chunkingBehavior === ChunkingBehaviors.none) {
+export default function chunkText(
+  input: string,
+  maxChunkSize: number,
+  chunkingBehavior: ChunkingBehaviors,
+  balkingDistance: number
+) {
+  if (
+    chunkingBehavior === ChunkingBehaviors.none
+    || input.length === 0
+    ) {
     return [input];
   }
 
-  if (input.length === 0) {
-    return [''];
-  }
-
-  let remainingInput = input;
+  let remainingInput = input.trim();
   const chunks: string[] = [];
 
   while (remainingInput.length > 0) {
-    if (remainingInput.length > 500) {
-      const rawChunk = remainingInput.substring(0, 500);
-      let end = rawChunk.length - 1;
-      let distance = Infinity;
-
-      if (chunkingBehavior === ChunkingBehaviors.sentenceBoundary) {
-        let matches = Array.from(rawChunk.matchAll(/[.?!] /gm));
-
-        if (/[.?!] /gm.test(rawChunk)) {
-          end = matches.pop().index + 1;
-          distance = 500 - end;
-        }
-
-        if (distance > balkingDistance && rawChunk.includes(', ')) {
-          matches = Array.from(rawChunk.matchAll(/[,;-] /gm));
-
-          end = matches.pop().index + 1;
-          distance = 500 - end;
-        }
-      }
-
-      if (chunkingBehavior === ChunkingBehaviors.wordBoundary || distance > balkingDistance) {
-        if (rawChunk.includes(' ')) {
-          end = rawChunk.lastIndexOf(' ');
-        }
-      }
-
-      chunks.push(rawChunk.substring(0, end));
-      remainingInput = remainingInput
-        .substring(end)
-        .trim();
-    } else {
+    if (remainingInput.length <= maxChunkSize) {
       chunks.push(remainingInput);
       break;
     }
+
+    const rawChunk = remainingInput.substring(0, maxChunkSize);
+    let tentativeEndIndex = rawChunk.length - 1;
+    let distance = Infinity;
+
+    if (chunkingBehavior === ChunkingBehaviors.sentenceBoundary) {
+      let matches: RegExpMatchArray[];
+
+      if (hardSentenceBoundaryRegexp.test(rawChunk)) {
+        matches = Array.from(rawChunk.matchAll(hardSentenceBoundaryRegexp));
+        tentativeEndIndex = matches.pop().index;
+        distance = 500 - tentativeEndIndex + 1;
+      }
+
+      if (
+        distance > balkingDistance
+        && softSentenceBoundaryRegexp.test(rawChunk)
+      ) {
+        matches = Array.from(rawChunk.matchAll(softSentenceBoundaryRegexp));
+        tentativeEndIndex = matches.pop().index;
+        distance = 500 - tentativeEndIndex + 1;
+      }
+    }
+
+    if (
+      chunkingBehavior === ChunkingBehaviors.wordBoundary
+      || distance > balkingDistance
+    ) {
+      if (rawChunk.includes(' ')) {
+        tentativeEndIndex = rawChunk.lastIndexOf(' ');
+      }
+    }
+
+    chunks.push(rawChunk.substring(0, tentativeEndIndex + 1));
+    remainingInput = remainingInput
+      .substring(tentativeEndIndex + 1)
+      .trim();    
   }
 
   return chunks;
