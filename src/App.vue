bde4a9607decd7791712cf6504c0faa3c3d8c1e5
Christopher <ChrFimbel@gmail.com>
2022-07-26 09:48:16 -0600

    Re-added CheckboxInput.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { reactive } from 'vue';
 import BaseTextarea from './components/BaseTextarea.vue';
+import CheckboxInput from './components/CheckboxInput.vue';
 
 const inputState = reactive({
   input: '',
+  removeCitations: true,
+  removeLineTerminators: true,
+  removeParentheticals : true,
 });
 </script>
 
@@ -12,5 +16,20 @@ const inputState = reactive({
     id="input"
     label="Input"
     v-model="inputState.input"
+  />
+  <CheckboxInput
+    id="remove-citations"
+    v-model="inputState.removeCitations"
+    label="Remove Citations"
+  />
+  <CheckboxInput
+    id="remove-line-terminators"
+    v-model="inputState.removeLineTerminators"
+    label="Remove Line Terminators"
+  />
+  <CheckboxInput
+    id="remove-parentheticals"
+    v-model="inputState.removeParentheticals"
+    label="Remove Parentheticals"
   />
 </template>
---
+++ src/components/CheckboxInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseInput
+    :id="id"
+    :label="label"
+    :model-value="modelValue"
+    type="checkbox"
+    @update:model-value="onUpdateModelValue"
+  />
+</template>
+
+<script setup lang="ts">
+import BaseInput from './BaseInput.vue';
+
+defineProps<{
+  id: string,
+  label: string,
+  modelValue: boolean,
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void,
+}>();
+
+function onUpdateModelValue(value: boolean | string) {
+  if (typeof value === 'string') {
+    return;
+  }
+
+  emit('update:modelValue', value);
+}
+</script>
