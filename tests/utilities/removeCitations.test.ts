e9748b9e2bb5486bd156b176fb1dddd0b60b47da
Christopher <ChrFimbel@gmail.com>
2022-07-31 14:15:48 -0600

    Add tests for removeCitations.

---
+++ tests/utilities/removeCitations.test.ts
@@ -0,0 +1,16 @@
+import { removeCitations } from '../../src/utilities/removeCitations';
+import { describe, expect, it } from 'vitest';
+
+describe('removeCitations.ts', () => {
+  it('should remove matching square brackets and everything between them', () => {
+    expect(removeCitations('foo[1]')).toBe('foo');
+  });
+
+  it('should remove multiple sets of matching square brackets and everything between them', () => {
+    expect(removeCitations('foo[1] bar[2]')).toBe('foo bar');
+  });
+
+  it('should not remove non-matching square brackets', () => {
+    expect(removeCitations('foo]1[')).toBe('foo]1[');
+  });
+});
