eab3fc32b8a955d95cc5fb6a5bcb92abd4ff21ab
Christopher <ChrFimbel@gmail.com>
2022-11-18 20:22:22 -0700

    Remove getNextChunk.

--- src/utilities/chunkText.ts
+++ src/utilities/chunkText.ts
@@ -1,4 +1,8 @@
-import getNextChunk from './getNextChunk';
+import ChunkingBehavior from '@/types/ChunkingBehavior';
+import getNextChunkByChunkSize from './getNextChunkByChunkSize';
+import getNextChunkByHardSentenceBoundary from './getNextChunkByHardSentenceBoundary';
+import getNextChunkBySoftSentenceBoundary from './getNextChunkBySoftSentenceBoundary';
+import getNextChunkByWordBoundary from './getNextChunkByWordBoundary';
 
 export default function chunkText(
   input: string,
@@ -19,7 +23,20 @@ export default function chunkText(
       break;
     }
 
-    const nextChunk = getNextChunk(remainingInput, maxChunkSize, chunkingBehavior, balkingDistance);
+    let nextChunk: string;
+
+    if (chunkingBehavior === ChunkingBehavior.none) {
+      nextChunk = remainingInput;
+    } else if (chunkingBehavior === ChunkingBehavior.chunkSize) {
+      nextChunk = getNextChunkByChunkSize(remainingInput, maxChunkSize);
+    } else if (chunkingBehavior === ChunkingBehavior.wordBoundary) {
+      nextChunk = getNextChunkByWordBoundary(remainingInput, maxChunkSize, balkingDistance);
+    } else if (chunkingBehavior === ChunkingBehavior.softSentenceBoundary) {
+      nextChunk = getNextChunkBySoftSentenceBoundary(remainingInput, maxChunkSize, balkingDistance);
+    } else {
+      nextChunk = getNextChunkByHardSentenceBoundary(remainingInput, maxChunkSize, balkingDistance);
+    }
+
     chunks.push(nextChunk);
 
     remainingInput = remainingInput
